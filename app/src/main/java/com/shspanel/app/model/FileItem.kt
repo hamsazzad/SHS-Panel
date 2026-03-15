333ada72a58e4a38e3357dcb029cc3ac86208c33
hamsazzad <shobujkhanshs520@gmail.com>
2026-03-16 01:55:03 +0600

    Add source: FileItem.kt

---
+++ app/src/main/java/com/shspanel/app/model/FileItem.kt
@@ -0,0 +1,13 @@
+package com.shspanel.app.model
+
+import java.io.File
+
+data class FileItem(
+    val file: File,
+    val name: String = file.name,
+    val path: String = file.absolutePath,
+    val isDirectory: Boolean = file.isDirectory,
+    val size: Long = if (file.isFile) file.length() else 0L,
+    val lastModified: Long = file.lastModified(),
+    val extension: String = file.extension.lowercase()
+)
